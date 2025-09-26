f2567611bd4bea69fd5d105715d7b71b8861e969
BoduBhai <bodruddozaaraf@gmail.com>
2025-09-27 02:02:12 +0600

    Get all category and deleting category

--- src/components/providers.tsx
+++ src/components/providers.tsx
@@ -2,21 +2,28 @@
 
 import { ReactNode } from "react";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Toaster } from "./ui/sonner";
 
 type ProvidersProps = {
   children: ReactNode;
 };
 
+const queryClient = new QueryClient();
+
 const Providers = ({ children }: ProvidersProps) => {
   return (
-    <NextThemesProvider
-      attribute="class"
-      defaultTheme="system"
-      enableSystem
-      disableTransitionOnChange
-    >
-      {children}
-    </NextThemesProvider>
+    <QueryClientProvider client={queryClient}>
+      <NextThemesProvider
+        attribute="class"
+        defaultTheme="system"
+        enableSystem
+        disableTransitionOnChange
+      >
+        <Toaster />
+        {children}
+      </NextThemesProvider>
+    </QueryClientProvider>
   );
 };
 
